44fb8c25194d8c946c5347756427d9e38a2d664c
Ravi Kandhadai <rkandhadaimadhav@apple.com>
2020-02-01 18:37:23 -0800

    Fixing OSLogPrototypeFullOptTest for 32bit architecture

--- test/SILOptimizer/OSLogPrototypeFullOptTest.swift
+++ test/SILOptimizer/OSLogPrototypeFullOptTest.swift
@@ -26,21 +26,25 @@ func testSimpleInterpolation(h: Logger) {
     //
     // Header bytes.
     //
-    // CHECK-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i64 12
+    // CHECK-64-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i64 12
+    // CHECK-32-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i32 8
     // CHECK-NEXT: store i8 0, i8* [[BUFFER]], align 1
-    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 1
+    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 1
     // CHECK-NEXT: store i8 1, i8* [[OFFSET1]], align 1
     //
     // Argument bytes.
     //
-    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 2
+    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 2
     // CHECK-NEXT: store i8 2, i8* [[OFFSET2]], align 1
-    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 3
-    // CHECK-NEXT: store i8 8, i8* [[OFFSET3]], align 1
-    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 4
-    // CHECK-NEXT: [[BITCASTED:%.+]] = bitcast i8* [[OFFSET4]] to i64*
-    // CHECK-NEXT: store i64 -9223372036854775808, i64* [[BITCASTED]], align 1
-    // CHECK-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([36 x i8], [36 x i8]* @{{.*}}, i64 0, i64 0), i8* [[BUFFER]], i32 12)
+    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 3
+    // CHECK-64-NEXT: store i8 8, i8* [[OFFSET3]], align 1
+    // CHECK-32-NEXT: store i8 4, i8* [[OFFSET3]], align 1
+    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 4
+    // CHECK-NEXT: [[BITCASTED:%.+]] = bitcast i8* [[OFFSET4]] to i{{.*}}*
+    // CHECK-64-NEXT: store i64 -9223372036854775808, i64* [[BITCASTED]], align 1
+    // CHECK-32-NEXT: store i32 -2147483648, i32* [[BITCASTED]], align 1
+    // CHECK-64-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([36 x i8], [36 x i8]* @{{.*}}, i64 0, i64 0), i8* [[BUFFER]], i32 12)
+    // CHECK-32-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([34 x i8], [34 x i8]* @{{.*}}, i32 0, i32 0), i8* [[BUFFER]], i32 8)
     // CHECK-NEXT: tail call void @swift_slowDealloc(i8* [[BUFFER]]
     // CHECK-NEXT: br label %[[NOT_ENABLED]]
 
@@ -53,9 +57,9 @@ func testSimpleInterpolation(h: Logger) {
 // CHECK-LABEL: define hidden swiftcc void @"${{.*}}testInterpolationWithMultipleArguments
 @available(OSX 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
 func testInterpolationWithMultipleArguments(h: Logger) {
-  let privateID = 0x79abcdef
-  let filePermissions = 0o777
-  let pid = 122225
+  let privateID: Int32 = 0x79abcdef
+  let filePermissions: Int32 = 0o777
+  let pid: Int32 = 122225
   h.log(
     level: .error,
     """
@@ -74,43 +78,43 @@ func testInterpolationWithMultipleArguments(h: Logger) {
     //
     // Header bytes.
     //
-    // CHECK-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i64 32
+    // CHECK-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i{{.*}} 20
     // CHECK-NEXT: store i8 1, i8* [[BUFFER]], align 1
-    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 1
+    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 1
     // CHECK-NEXT: store i8 3, i8* [[OFFSET1]], align 1
     //
     // First argument bytes.
     //
-    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 2
+    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 2
     // CHECK-NEXT: store i8 2, i8* [[OFFSET2]], align 1
-    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 3
-    // CHECK-NEXT: store i8 8, i8* [[OFFSET3]], align 1
-    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 4
-    // CHECK-NEXT: [[BITCASTED:%.+]] = bitcast i8* [[OFFSET4]] to i64*
-    // CHECK-NEXT: store i64 122225, i64* [[BITCASTED]], align 1
+    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 3
+    // CHECK-NEXT: store i8 4, i8* [[OFFSET3]], align 1
+    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 4
+    // CHECK-NEXT: [[BITCASTED:%.+]] = bitcast i8* [[OFFSET4]] to i32*
+    // CHECK-NEXT: store i32 122225, i32* [[BITCASTED]], align 1
     //
     // Second argument
     //
-    // CHECK-NEXT: [[OFFSET12:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 12
+    // CHECK-NEXT: [[OFFSET12:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 8
     // CHECK-NEXT: store i8 1, i8* [[OFFSET12]], align 1
-    // CHECK-NEXT: [[OFFSET13:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 13
-    // CHECK-NEXT: store i8 8, i8* [[OFFSET13]], align 1
-    // CHECK-NEXT: [[OFFSET14:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 14
-    // CHECK-NEXT: [[BITCASTED2:%.+]] = bitcast i8* [[OFFSET14]] to i64*
-    // CHECK-NEXT: store i64 2041302511, i64* [[BITCASTED2]], align 1
+    // CHECK-NEXT: [[OFFSET13:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 9
+    // CHECK-NEXT: store i8 4, i8* [[OFFSET13]], align 1
+    // CHECK-NEXT: [[OFFSET14:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 10
+    // CHECK-NEXT: [[BITCASTED2:%.+]] = bitcast i8* [[OFFSET14]] to i32*
+    // CHECK-NEXT: store i32 2041302511, i32* [[BITCASTED2]], align 1
     //
     // Third argument
     //
-    // CHECK-NEXT: [[OFFSET22:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 22
+    // CHECK-NEXT: [[OFFSET22:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 14
     // CHECK-NEXT: store i8 2, i8* [[OFFSET22]], align 1
-    // CHECK-NEXT: [[OFFSET23:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 23
-    // CHECK-NEXT: store i8 8, i8* [[OFFSET23]], align 1
-    // CHECK-NEXT: [[OFFSET24:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 24
-    // CHECK-NEXT: [[BITCASTED3:%.+]] = bitcast i8* [[OFFSET24]] to i64*
-    // CHECK-NEXT: store i64 511, i64* [[BITCASTED3]], align 1
+    // CHECK-NEXT: [[OFFSET23:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 15
+    // CHECK-NEXT: store i8 4, i8* [[OFFSET23]], align 1
+    // CHECK-NEXT: [[OFFSET24:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 16
+    // CHECK-NEXT: [[BITCASTED3:%.+]] = bitcast i8* [[OFFSET24]] to i32*
+    // CHECK-NEXT: store i32 511, i32* [[BITCASTED3]], align 1
     //
     // os_log_impl call.
-    // CHECK-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([120 x i8], [120 x i8]* @{{.*}}, i64 0, i64 0), i8* [[BUFFER]], i32 32)
+    // CHECK-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([114 x i8], [114 x i8]* @{{.*}}, i{{.*}} 0, i{{.*}} 0), i8* [[BUFFER]], i32 20)
     // CHECK-NEXT: tail call void @swift_slowDealloc(i8* [[BUFFER]]
     // CHECK-NEXT: br label %[[NOT_ENABLED]]
 
@@ -137,22 +141,25 @@ func testNSObjectInterpolation(h: Logger, nsArray: NSArray) {
     //
     // Header bytes.
     //
-    // CHECK-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i64 12
+    // CHECK-64-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i64 12
+    // CHECK-32-NEXT: [[BUFFER:%.+]] = tail call noalias i8* @swift_slowAlloc(i32 8
     // CHECK-NEXT: store i8 2, i8* [[BUFFER]], align 1
-    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 1
+    // CHECK-NEXT: [[OFFSET1:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 1
     // CHECK-NEXT: store i8 1, i8* [[OFFSET1]], align 1
     //
     // Argument bytes.
     //
-    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 2
+    // CHECK-NEXT: [[OFFSET2:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 2
     // CHECK-NEXT: store i8 66, i8* [[OFFSET2]], align 1
-    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 3
-    // CHECK-NEXT: store i8 8, i8* [[OFFSET3]], align 1
-    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i64 4
+    // CHECK-NEXT: [[OFFSET3:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 3
+    // CHECK-64-NEXT: store i8 8, i8* [[OFFSET3]], align 1
+    // CHECK-32-NEXT: store i8 4, i8* [[OFFSET3]], align 1
+    // CHECK-NEXT: [[OFFSET4:%.+]] = getelementptr inbounds i8, i8* [[BUFFER]], i{{.*}} 4
     // CHECK-NEXT: [[BITCASTED_DEST:%.+]] = bitcast i8* [[OFFSET4]] to %TSo7NSArrayC**
     // CHECK-NEXT: [[BITCASTED_SRC:%.+]] = bitcast i8* [[NSARRAY_ARG]] to %TSo7NSArrayC*
     // CHECK-NEXT: store %TSo7NSArrayC*  [[BITCASTED_SRC]], %TSo7NSArrayC** [[BITCASTED_DEST]], align 1
-    // CHECK-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([20 x i8], [20 x i8]* @{{.*}}, i64 0, i64 0), i8* [[BUFFER]], i32 12)
+    // CHECK-64-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([20 x i8], [20 x i8]* @{{.*}}, i64 0, i64 0), i8* [[BUFFER]], i32 12)
+    // CHECK-32-NEXT: tail call void @_os_log_impl({{.*}}, {{.*}} [[LOGOBJ]], i8 zeroext [[LOGLEVEL]], i8* getelementptr inbounds ([20 x i8], [20 x i8]* @{{.*}}, i32 0, i32 0), i8* [[BUFFER]], i32 8)
     // CHECK-NEXT: tail call void @swift_slowDealloc(i8* [[BUFFER]]
     // CHECK-NEXT: br label %[[NOT_ENABLED]]
 
